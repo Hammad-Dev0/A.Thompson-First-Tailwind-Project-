56d4808e874d6287f8fe4b069dc1b2aca52313ea
Hammad Dev <hammaddev00@gmail.com>
2026-02-09 09:09:14 +0500

    readme.md are commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+  const btn = document.getElementById("menuBtn");
+  const menu = document.getElementById("mobileMenu");
+
+  btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
